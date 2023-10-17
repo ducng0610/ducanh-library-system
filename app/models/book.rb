1e9c3f19310bd84f6f3fc2524eff41db5962de5a
Duc Anh Nguyen <duc4nh.nguyen@gmail.com>
2023-10-17 17:47:45 +0700

    Add model validation and association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :subject
+
+  validates_presence_of :title, :author, :publisher
+  validates_uniqueness_of :title
+  validates_numericality_of :year
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_103553) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_050510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_103553) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "publisher"
+    t.integer "year"
+    t.datetime "borrowed_at"
+    t.boolean "available"
+    t.bigint "subject_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_books_on_subject_id"
   end
 
   create_table "class_rooms", force: :cascade do |t|
@@ -108,4 +121,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_103553) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "books", "subjects"
 end
